52dbc851019ec4734d726095d80faa809bb034b1
kosven <sven.koesling@library.ethz.ch>
2017-01-04 17:38:01 +0100

    peak eingebaut

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :attendances
   has_many :plans, through: :attendances
+
+  has_many :participations
+  has_many :milestones, through: :participations
     
   scope :aktuell,->{where(geloescht: false)}
 
@@ -13,6 +16,10 @@ class User < ActiveRecord::Base
       self.blockdaten = "24.12. - 31.12."
     end
     self.save
+  end
+
+  def mpeak(mid)
+    self.participations.where(milestone_id: mid).first.peak
   end
 
 end
